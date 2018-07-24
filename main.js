87df4a4f237d27542ffb9fb7b30fc7a368da2d4e
Katy Gibson <klgibson528@gmail.com>
2018-07-24 17:20:51 -0500

    loading

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
+// Adjust the "querySelector" value to target your image
+var img = document.querySelector("img");
 document.body.classList.add("js-loading");
-
-window.addEventListener("load", showPage);
-
-function showPage() {
+img.addEventListener("load", removeLoadingClass);
+function removeLoadingClass() {
   document.body.classList.remove("js-loading");
 }
-
-// // Adjust the "querySelector" value to target your image
-// var img = document.querySelector("img");
-// document.body.classList.add("js-loading");
-// img.addEventListener("load", removeLoadingClass);
-// function removeLoadingClass() {
-//   document.body.classList.remove("js-loading");
-// }
 
 function ShowDiv() {
   var div = document.getElementById("more-info");
