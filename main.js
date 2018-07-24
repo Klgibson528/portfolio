0c6c8aeb528ee07a11f73bdabe7e6d09e79f7c56
Katy Gibson <klgibson528@gmail.com>
2018-07-23 19:16:55 -0500

    basically finished minus email

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ function showPage() {
 //   document.body.classList.remove("js-loading");
 // }
 
+function ShowDiv() {
+  var div = document.getElementById("more-info");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
+
+$(window).on("load", function() {
+  var viewportWidth = $(window).width();
+  if (viewportWidth < 1200) {
+    $(".typewriter").removeClass("typewriter");
+  }
+});
